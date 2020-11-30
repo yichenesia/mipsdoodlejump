b35d5d481cd67e41c538f9834932eb620823334a
Yichen (Chloe) Zhao <yichen.zhao@mail.utoronto.ca>
2020-11-30 17:51:09 -0500

    Update doodlejump.s

--- doodlejump.s
+++ doodlejump.s
@@ -9,20 +9,46 @@
 #
 .data
 	displayAddress: .word 0x10008000
+	colorone: .word 0xff0000
+	colortwo: .word 0x00ff00
+	colorthree: .word 0x0000ff
+	
 .text
 	lw $t0, displayAddress # $t0 stores the base address for display
-	li $t1, 0xff0000 # $t1 stores the red colour code
-	li $t2, 0x00ff00 # $t2 stores the green colour code
-	li $t3, 0x0000ff # $t3 stores the blue colour code
-	add $t5, $zero, $zero
-	addi $t6, $zero, 8
-	sw $t1, 0($t0) # paint the first (top-left) unit red.
-	
-	addi $t0, $t0, 8
-	
-	sw $t2, 0($t0) # paint the second unit on the first row green. Why $t0+4?
-	
-	sw $t3, 128($t0) # paint the first unit on the second row blue. Why +128?
+	lw $t5, colorone # $t1 stores the red colour code
+
+DrawBranch: 	
+	add $t1, $zero, $zero # Loop increment variable, i
+      	addi $t2, $zero, 5 # Length of a platform
+      	addi $t0, $t0, 144
+LOOP: 	beq $t1, $t2, DrawBranchTwo
+	addi $t0, $t0, 4
+	sw $t5, 0($t0)
+	addi $t1, $t1, 1
+	j LOOP
+
+DrawBranchTwo: 	
+	add $t1, $zero, $zero # Loop increment variable, i
+      	addi $t2, $zero, 5 # Length of a platform
+ 	addi $t0, $t0, 1024
+LOOPTwo:
+	beq $t1, $t2, DrawBranchThree
+	addi $t0, $t0, 4
+	sw $t5, 0($t0)
+	addi $t1, $t1, 1
+	j LOOPTwo
+
+DrawBranchThree: 	
+	add $t1, $zero, $zero # Loop increment variable, i
+      	addi $t2, $zero, 5 # Length of a platform
+ 	addi $t0, $t0, 2048
+LOOPThree:
+	beq $t1, $t2, Exit
+	addi $t0, $t0, 4
+	sw $t5, 0($t0)
+	addi $t1, $t1, 1
+	j LOOPThree
+
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
