730608c3fba442dd79e86e147e9149b5a4f6a199
Yichen (Chloe) Zhao <yichen.zhao@mail.utoronto.ca>
2020-12-01 15:15:30 -0500

    Added keyboard input and left/right movement

--- doodlejump.s
+++ doodlejump.s
@@ -13,30 +13,63 @@
 	colortwo: .word 0x8AB8FF
 	colorthree: .word 0xD8FFEC
 	
+	platformone: .word 160
+	platformtwo: .word 1168
+	platformthree: .word 3136
 	
+	doodler: .word 128
 .text
 	lw $t0, displayAddress # $t0 stores the base address for display
 	lw $s0, colorone # $s0 stores the orange colour code
 	lw $s1, colortwo # $s1 stores the blue colour code
 	lw $s2, colorthree # $s2 stores the green colour code
 
-Draw: 	
-	add $a0, $t0, $zero
-	addi $a1, $zero, 4092
+main: 	# Check for Keyboard Input
+	lw $t8, 0xffff0000
+	beq $t8, 1, KeyboardInput # Update location of doodler (And potentially platforms?)
+	
+	# Redraw screen
+Draw:	add $a0, $t0, $zero # Store the display address into an argument function
+	addi $a1, $zero, 4092 # The max size 
 	jal DrawBackground
 	
-	addi $a0, $t0, 160 # Initialize the input argument to the function, the leftmost element of the branch
-	jal DrawBranch	# Draw the branch
-	addi $a0, $t0, 1168 
-	jal DrawBranch
-	addi $a0, $t0, 3136 
-	jal DrawBranch
-	addi $a0, $t0, 2752
+	lw $t1, platformone # Load the offset location of the branch into a register
+	add $a0, $t0, $t1 # Add the offset to the displayAddress, and store it as an argument
+	jal DrawPlatform # Draw the branch
+	lw $t1, platformtwo
+	add $a0, $t0, $t1
+	jal DrawPlatform
+	lw $t1, platformthree
+	add $a0, $t0, $t1
+	jal DrawPlatform
+	
+	lw $t1, doodler 
+	add $a0, $t0, $t1
 	jal DrawDoodler
 	
-	j Exit
-
-
+	# Sleep
+	li $v0, 32
+	li $a0, 1000
+	syscall
+	
+	# Continue looping
+	j  main
+KeyboardInput: 
+	lw $t2, 0xffff0004 # Load the keyboard input ASCII code
+	beq $t2, 0x6A, MoveLeft # If the ASCII code corresponds to j
+	beq $t2, 0x6B, MoveRight # If the ASCII Code corresponds to k
+	beq $t2, 0x78, Exit # Exit game when x is pressed
+MoveLeft:
+	lw $t1, doodler
+	subiu $t2, $t1, 4
+	sw $t2, doodler
+	j Draw
+MoveRight:
+	lw $t1, doodler
+	addi $t2, $t1, 4
+	sw $t2, doodler
+	j Draw
+	
 DrawBackground:
 	add $t1, $zero, $zero # Initialize increment variable i
 L1:	beq $t1, $a1, FinishBG
@@ -48,10 +81,10 @@ FinishBG:
 	jr $ra
 
 	
-DrawBranch: 	
+DrawPlatform: 	
 	add $t1, $zero, $zero # Loop increment variable, i
       	addi $t2, $zero, 5 # Length of a platform
-L2: 	beq $t1, $t2, FinishBranch # If i == 5, exit the loop
+L2: 	beq $t1, $t2, FinishPlatform # If i == 5, exit the loop
 	beqz $t1 IF # If i == 0, do not increment by 4
 	addi $a0, $a0, 4 # Increment the "location pointer" by 4 to create another pixel
 	sw $s0, 0($a0) # Load it into memory
@@ -60,9 +93,8 @@ L2: 	beq $t1, $t2, FinishBranch # If i == 5, exit the loop
 IF:	sw $s0, 0($a0)
 	addi $t1, $t1, 1
 	j L2
-FinishBranch:	
+FinishPlatform:	
 	jr $ra # Jump back to Draw
-
 
 DrawDoodler: 
 	sw $s1, 4($a0)
