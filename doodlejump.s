5727b3d2b510ce882963dd859dabb80677416400
Yichen (Chloe) Zhao <yichen.zhao@mail.utoronto.ca>
2020-11-30 19:15:29 -0500

    Changed colors

--- doodlejump.s
+++ doodlejump.s
@@ -9,69 +9,68 @@
 #
 .data
 	displayAddress: .word 0x10008000
-	colorone: .word 0xff0000
-	colortwo: .word 0x00ff00
-	colorthree: .word 0x0000ff
+	colorone: .word 0xFFCA8A
+	colortwo: .word 0x8AB8FF
+	colorthree: .word 0xD8FFEC
+	
 	
 .text
 	lw $t0, displayAddress # $t0 stores the base address for display
-	lw $s0, colorone # $t1 stores the red colour code
-	lw $s1, colortwo
+	lw $s0, colorone # $s0 stores the orange colour code
+	lw $s1, colortwo # $s1 stores the blue colour code
+	lw $s2, colorthree # $s2 stores the green colour code
 
 Draw: 	
+	add $a0, $t0, $zero
+	addi $a1, $zero, 4092
+	jal DrawBackground
+	
 	addi $a0, $t0, 160 # Initialize the input argument to the function, the leftmost element of the branch
 	jal DrawBranch	# Draw the branch
-	addi $a0, $t0, 1168 # Initialize the input argument to the function, the leftmost element of the branch
+	addi $a0, $t0, 1168 
 	jal DrawBranch
-	addi $a0, $t0, 3136 # Initialize the input argument to the function, the leftmost element of the branch
+	addi $a0, $t0, 3136 
 	jal DrawBranch
+	addi $a0, $t0, 2752
+	jal DrawDoodler
+	
 	j Exit
 
 
+DrawBackground:
+	add $t1, $zero, $zero # Initialize increment variable i
+L1:	beq $t1, $a1, FinishBG
+	sw $s2, 0($a0)
+	addi $a0, $a0, 4
+	addi $t1, $t1, 1
+	j L1
+FinishBG:	
+	jr $ra
+
+	
 DrawBranch: 	
 	add $t1, $zero, $zero # Loop increment variable, i
       	addi $t2, $zero, 5 # Length of a platform
-LOOP: 	beq $t1, $t2, End
-	beqz $t1 IF
-	addi $a0, $a0, 4
-	sw $s0, 0($a0)
-	addi $t1, $t1, 1
-	j LOOP
+L2: 	beq $t1, $t2, FinishBranch # If i == 5, exit the loop
+	beqz $t1 IF # If i == 0, do not increment by 4
+	addi $a0, $a0, 4 # Increment the "location pointer" by 4 to create another pixel
+	sw $s0, 0($a0) # Load it into memory
+	addi $t1, $t1, 1 # Increment i
+	j L2 # Continue looping
 IF:	sw $s0, 0($a0)
 	addi $t1, $t1, 1
-	j LOOP
-End:	jr $ra
+	j L2
+FinishBranch:	
+	jr $ra # Jump back to Draw
 
-
-DrawBranchTwo: 	
-	add $t1, $zero, $zero # Loop increment variable, i
-      	addi $t2, $zero, 5 # Length of a platform
- 	addi $t0, $t0, 1024
-LOOPTwo:
-	beq $t1, $t2, DrawBranchThree
-	addi $t0, $t0, 4
-	sw $t5, 0($t0)
-	addi $t1, $t1, 1
-	j LOOPTwo
-
-DrawBranchThree: 	
-	add $t1, $zero, $zero # Loop increment variable, i
-      	addi $t2, $zero, 5 # Length of a platform
- 	addi $t0, $t0, 2048
-LOOPThree:
-	beq $t1, $t2, DrawDoodler
-	addi $t0, $t0, 4
-	sw $t5, 0($t0)
-	addi $t1, $t1, 1
-	j LOOPThree
 
 DrawDoodler: 
-	lw $t0, displayAddress
-	sw $t6, 4($t0)
-	sw $t6, 128($t0)
-	sw $t6, 136($t0)
-	sw $t6, 256($t0)
-	sw $t6, 264($t0)
+	sw $s1, 4($a0)
+	sw $s1, 128($a0)
+	sw $s1, 136($a0)
+	sw $s1, 256($a0)
+	sw $s1, 264($a0)
+	jr $ra
 	
 Exit:
 	li $v0, 10 # terminate the program gracefully
